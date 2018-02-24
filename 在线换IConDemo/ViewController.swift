b4d50e7baf8add5f5ed90feab21e74d821bc3acd
3irobotics <dingfd1127@3irobotics.com>
2018-02-24 10:11:56 +0800

    在线更换ICon

--- 在线换IConDemo/ViewController.swift
+++ 在线换IConDemo/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var currentIndex: Int = 0
+    
+    // 1， 在系统跟目录加上需要更换图标的图片
+    // 2， 在info.plist中设置需要更换图片的信息
+    // 3， 添加扩展分类，通过runtime去掉更换图标的弹窗
+    private var weathers = ["icon_i", "icon_h", "icon_r"]
+    
+    @IBAction func change(_ sender: Any) {
+        let weather = self.weathers[currentIndex]
+        self.setAppIconWithName(weather)
+        currentIndex += 1
+        currentIndex = currentIndex > 2 ? 0 : currentIndex
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func setAppIconWithName(_ iconName: String){
+        if #available(iOS 10.3, *) {
+            if UIApplication.shared.supportsAlternateIcons {
+                print("支持更换图标！")
+                UIApplication.shared.setAlternateIconName(iconName, completionHandler: { (error) in
+                    if error != nil {
+                        print("替换icon失败\(String(describing: error?.localizedDescription))")
+                    }else{
+                        print("更换图标成功")
+                    }
+                })
+            }else {
+                print("设备不支持更换图标")
+            }
+        } else {
+            
+        }
     }
-
-
+    
 }
 
